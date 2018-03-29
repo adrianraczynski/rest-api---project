41f5dc248c919025b5dcd719b0bfcb591e0d5a32
“adrianraczynski <adrianraczynski7@gmail.com>
2018-03-29 18:32:12 +0200

    zad 15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -2,33 +2,35 @@ package com.crud.tasks.controller;
 
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-
-    //private TaskMapper taskMapper;
-    //private DbServie dbServie;
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private DbService service;
 
     @RequestMapping (method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks(){
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping (method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask (String taskId) {
-        return new TaskDto ((long)1, "test title", "test_content");
+        return taskMapper.mapToTaskDto(service.getTaskById());
     }
 
     @RequestMapping (method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask (String taskId) {
-
     }
 
     @RequestMapping (method = RequestMethod.PUT, value = "updateTask")
@@ -38,6 +40,5 @@ public class TaskController {
 
     @RequestMapping (method = RequestMethod.POST, value = "createTask")
     public void createTask (TaskDto taskDto){
-
     }
 }
--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.crud.tasks;
+/** package com.crud.tasks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,4 +13,4 @@ public class TasksApplicationTests {
 	public void contextLoads() {
 	}
 
-}
+} **/
