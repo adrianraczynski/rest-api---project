00dcc579321f279b2b7002f5f2eaeb9b5b470c34
“adrianraczynski <adrianraczynski7@gmail.com>
2018-03-29 19:58:25 +0200

    moduł 15.3

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,13 +1,19 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
-    Task findTaskById();
+    Optional<Task> findTaskById(Long id);
+
+    @Override
+    Task save (Task task);
+
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,7 +18,11 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskById() {
-        return repository.findTaskById();
+    public Optional<Task> getTask(final Long id) {
+    return repository.findTaskById(id);
+    }
+
+    public Task saveTask (final Task task) {
+        return repository.save(task);
     }
 }
