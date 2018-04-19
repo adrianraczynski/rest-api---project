4a009409b3f786e52be7d1d7184666b76451ec09
adrianraczynski <adrianraczynski7@gmail.com>
2018-04-19 22:25:16 +0200

    zad 19.1-heroku

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication /** extends SpringBootServletInitializer **/ {
+public class TasksApplication  extends SpringBootServletInitializer  {		// extands.....	musi być zakomentowane, ponieważ dla modułu 19.1 odkomentowałee
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	/**
-	@Override
+
+	@Override		// ta metoda musi być zakomentowana, ponieważ dla modułu 19.1 odkomentowałem ją
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
-	}	**/
+	}
 }
