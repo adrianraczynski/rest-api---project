312a643c1af5b8feb043978cf885583652fb9d2f
adrianraczynski <adrianraczynski7@gmail.com>
2018-05-25 21:52:15 +0200

    Zad 24.2

---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CompanyConfig {
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
+    @Value("${info.company.signature}")
+    private String companySignature;
+
+
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+
+}
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,15 +1,15 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-//import org.apache.commons.lang3.StringUtils;
-
 
 @Service
 public class SimpleEmailService {
@@ -19,18 +19,27 @@ public class SimpleEmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send (final Mail mail) {
         LOGGER.info("Starting email preparation...");
 
         try {
-
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
             LOGGER.info("Email has been sent.");
-
         } catch (MailException e) {
             LOGGER.error("Failed to process email sending", e.getMessage(), e);
         }
+
+        /**  dla modułu 19
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+            LOGGER.info("Email has been sent.");
+        } catch (MailException e) {
+            LOGGER.error("Failed to process email sending", e.getMessage(), e);
+        }       **/
     }
 
     private SimpleMailMessage createMailMessage (final Mail mail) {
@@ -39,15 +48,34 @@ public class SimpleEmailService {
 
         if (mail.getToCc() == null) {
             LOGGER.info("Cc field is empty!");
-
         } else {
             mailMessage.setCc(mail.getToCc());
             LOGGER.info("The Cc field has been filled out.");
         }
 
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));        //dla modułu 24.2
+
+        //dla modułu 19
+        //mailMessage.setText(mail.getMessage());
 
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+
+            if (mail.getToCc() == null) {
+                LOGGER.info("Cc field is empty!");
+            } else {
+                messageHelper.setCc(mail.getToCc());
+                LOGGER.info("The Cc field has been filled out.");
+            }
+
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 }
